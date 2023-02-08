1661cb6e7f948452d4113e26ca19921ad440fe19
ATFA <AsThingsFallApart@gmail.com>
2023-02-07 20:32:26 -0500

    change color click to be a continuous toggle

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ import * as THREE from "three";
 main();
 
 function main() {
-  let cubeColor = "#ED36E9";
+  let redAscending = false;
+  let blueAscending = false;
+  let greenAscending = false;
+  let cubeColor = [237.0, 54.0, 233.0];
+  let allowColorChanging = false;
   const canvas = document.querySelector("#c");
   console.log(`App.js loaded.`);
 
@@ -60,15 +64,13 @@ function main() {
   let geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
   // create a material for the box
-  let material = new THREE.MeshPhongMaterial({ color: cubeColor });
+  let material = new THREE.MeshPhongMaterial({ color: rgbToHex(cubeColor) });
 
   let cube = new THREE.Mesh(geometry, material);
 
   scene.add(cube);
 
   renderer.render(scene, camera);
-
-  requestAnimationFrame(render);
 
   let color = 0xffffff;
   let intensity = 1;
@@ -78,20 +80,66 @@ function main() {
 
   let interactableArea = document.getElementsByClassName("interactableArea")[0];
 
-  interactableArea.addEventListener("click", changeColor);
+  interactableArea.addEventListener("click", toggleColorChanging);
+  interactableArea.addEventListener("scroll", scaleSize);
 
-  function changeColor(event) {
-    // generate a random color
-    cubeColor =
-      "#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substring(1, 7);
+  requestAnimationFrame(render);
+
+  function scaleSize(event) {}
+
+  function toggleColorChanging() {
     console.log(`Click received.`);
-    console.log(`\tThe new color: ${cubeColor}.`);
+    allowColorChanging
+      ? (allowColorChanging = false)
+      : (allowColorChanging = true);
+    console.log(`\tallowColorChanging: ${allowColorChanging}`);
 
-    // change color
-    material.color.set(cubeColor);
+    changeColor();
+  }
 
-    // EPILEPSY WARNING
-    // requestAnimationFrame(changeColor);
+  function changeColor() {
+    if (allowColorChanging) {
+      // go up and down red, green, or blue
+      console.log(
+        `Intial RGB values: ${cubeColor[0]}, ${cubeColor[1]}, ${cubeColor[2]}`
+      );
+
+      if (cubeColor[0] < 10.0) {
+        // start ascending red values
+        redAscending = true;
+      }
+      if (cubeColor[0] > 250.0) {
+        redAscending = false;
+      }
+
+      if (cubeColor[1] < 10.0) {
+        blueAscending = true;
+      }
+      if (cubeColor[1] > 250.0) {
+        blueAscending = false;
+      }
+
+      if (cubeColor[2] < 10.0) {
+        greenAscending = true;
+      }
+      if (cubeColor[2] > 250.0) {
+        greenAscending = false;
+      }
+
+      redAscending ? ++cubeColor[0] : --cubeColor[0];
+      blueAscending ? ++cubeColor[1] : --cubeColor[1];
+      greenAscending ? ++cubeColor[2] : --cubeColor[2];
+
+      console.log(`\tThe new color: ${cubeColor}.`);
+
+      // change color
+      material.color.set(rgbToHex(cubeColor));
+
+      // EPILEPSY WARNING @ low delays
+      setTimeout(() => {
+        requestAnimationFrame(changeColor);
+      }, "10");
+    }
   }
 
   function render(time) {
@@ -104,5 +152,14 @@ function main() {
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
+  }
+
+  function rgbToHex(rgb) {
+    let conversion = rgb.map(function (x) {
+      x = parseInt(x).toString(16);
+      return x.length == 1 ? "0" + x : x;
+    });
+
+    return "#" + conversion.join("");
   }
 }
